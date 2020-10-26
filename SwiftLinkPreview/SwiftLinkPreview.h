9f3fbdd0e9a053e66660828d0bc20644a3e29115
kay <ickang@enliple.com>
2020-10-26 17:07:13 +0900

    test

---
+++ SwiftLinkPreview/SwiftLinkPreview.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLinkPreview.h
+//  SwiftLinkPreview
+//
+//  Created by kay on 2020/10/26.
+//  Copyright © 2020 leocardz.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLinkPreview.
+FOUNDATION_EXPORT double SwiftLinkPreviewVersionNumber;
+
+//! Project version string for SwiftLinkPreview.
+FOUNDATION_EXPORT const unsigned char SwiftLinkPreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLinkPreview/PublicHeader.h>
+
+
